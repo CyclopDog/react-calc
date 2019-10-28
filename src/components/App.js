5ad2b128b700a2fd444b3ab4ab3aa9eca468341c
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-10-28 16:18:58 -0300

    removed console log

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import calculate from '../logic/calculate'
 
 class App extends React.Component {
   render () {
-    console.log(calculate({ total: '3', next: '4' }, 'AC'))
     return (
       <div>
         <Display />
